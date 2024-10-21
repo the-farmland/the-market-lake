32d995ef9ab8851f86486c5ce1c15cb180e82802
the-farm <167935056+the-farmland@users.noreply.github.com>
2024-10-21 19:35:21 -0400

    Delete src/routes/dapartments/groceries_routes.py

--- src/routes/dapartments/groceries_routes.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from src.controllers.departments.groceries.the_groceries_controller import router as groceries_router
-
-groceries_route = APIRouter()
-
-# Include all routes from groceries_controller
-groceries_route.include_router(groceries_router)
